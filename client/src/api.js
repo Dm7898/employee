e847473eb8d72b4b66192594ac50f61b28def2c0
Dm7898 <98467453+Dm7898@users.noreply.github.com>
2024-11-12 12:25:41 +0530

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create an instance of Axios with the base URL
 export const api = axios.create({
-  baseURL: "https://employee-iel6.onrender.com/", // Base URL for your API
+  baseURL: "https://employee-iel6.onrender.com", // Base URL for your API
   headers: {
     "Content-Type": "application/json",
   },
