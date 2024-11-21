80f35eb4bd3c9eda3847230985d8564e4d19cfa0
rasool <rasooli7898>
2024-11-22 00:30:52 +0530

    updated sever fiels

--- client/src/api.js
+++ client/src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create an instance of Axios with the base URL
 export const api = axios.create({
-  baseURL: "http://localhost:5000", // Set the base URL
+  baseURL: "https://employee-1-po53.onrender.com", // Set the base URL
   headers: {
     "Content-Type": "application/json", // Default Content-Type header
   },
--- server/server.js
+++ server/server.js
@@ -16,7 +16,10 @@ const __dirname = path.dirname(__filename);
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://employee-2-az33.onrender.com",
+];
 
 const corsOptions = {
   origin: function (origin, callback) {
